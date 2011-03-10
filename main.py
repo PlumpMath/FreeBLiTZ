c3a84c41be6d113964cdae3ac86d94f10374d68a
Don Laursen <don@werecar.(none)>
2011-03-09 22:02:56 -0500

    First commit.

---
+++ main.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+from direct.showbase.ShowBase import ShowBase
+
+class FreeBLiTZ(ShowBase):
+
+    def __init__(self):
+        ShowBase.__init__(self)
+
+app = FreeBLiTZ()
+app.run()
