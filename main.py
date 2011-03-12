83c0129e9714d8f961a9f6d3f0d49981c2031eb2
Don Laursen <don@werecar.(none)>
2011-03-11 23:03:13 -0500

    First working animation test.

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 #!/usr/bin/python
 from direct.showbase.ShowBase import ShowBase
+from direct.actor.Actor import Actor
 
 class FreeBLiTZ(ShowBase):
 
     def __init__(self):
         ShowBase.__init__(self)
+
+        self.arm = Actor('armtest', {'walk': 'armtest-anim1'})
+        self.arm.reparentTo(self.render)
+        self.arm.loop('walk')
 
 app = FreeBLiTZ()
 app.run()
