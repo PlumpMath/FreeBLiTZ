a7eea589405a747366ec3d5a36ca5cb1f5cfc1cd
Don Laursen <don@werecar.(none)>
2011-03-19 17:01:58 -0400

    Got vertex-painted stage and UV-textured actor working.

--- main.py
+++ main.py
@@ -1,15 +1,23 @@
 #!/usr/bin/python
 from direct.showbase.ShowBase import ShowBase
 from direct.actor.Actor import Actor
+from pandac.PandaModules import PointLight
 
 class FreeBLiTZ(ShowBase):
 
     def __init__(self):
         ShowBase.__init__(self)
 
-        self.arm = Actor('armtest', {'walk': 'armtest-anim1'})
-        self.arm.reparentTo(self.render)
-        self.arm.loop('walk')
+        self.stage = self.loader.loadModel('models/sandbox')
+        self.stage.reparentTo(self.render)
+
+        self.arm = Actor('models/blockchar')
+        self.arm.reparentTo(self.stage)
+
+        self.light = PointLight('plight')
+        self.lightNP = self.stage.attachNewNode(self.light)
+        self.lightNP.setPos(5, 5, 5)
+        self.stage.setLight(self.lightNP)
 
 app = FreeBLiTZ()
 app.run()
